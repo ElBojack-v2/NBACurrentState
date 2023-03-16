8eff3f4e22fb63745b2d6fbbf59cd05d8f255fcd
Rémi LAV <rlav@velvetconsulting.com>
2023-03-16 14:09:13 +0100

    change get to only get ppg (function)

--- src/nbaAPI/getter.js
+++ src/nbaAPI/getter.js
@@ -20,3 +20,15 @@ async function getTeamInfo(id) {
     pointScored: Number(teamJson.team.record.items[0].stats[3].value.toFixed(1))
   };
 }
+
+function getTeamsInfo() {
+  let teamsInfo = []
+
+  for (i = 1; i <= process.env.MAX_TEAM_INFO; i++) {
+    getTeamInfo(i)
+    .then(teamInfo => teamsInfo.push(teamInfo))
+    .catch(err => {console.log(err); return []})
+  }
+
+  return teamsInfo.sort((a, b) => { a.pointScore/a.pointAgainst > b.pointScore/b.pointAgainst})
+}
