ca6db5f553ec423bf11e0207c22d6a0e5d2a8396
Rémi LAV <rlav@velvetconsulting.com>
2023-03-16 11:13:53 +0100

    add fetch getter function and change teamlogo function to get team info by id

--- src/nbaAPI/getter.js
+++ src/nbaAPI/getter.js
@@ -1,14 +1,22 @@
-async function getTeamsLogo() {
-    const res = await fetch("https://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams");
-    
-    if (res.status != 200) {
-        return []
-    }
-    
-    const teamsJson = await res.json();
-		console.log("process json...")
-    console.log(teamsJson.sports[0].leagues[0].teams[0].team.logos[0].href)
-    const teamsLogo = teamsJson.sports[0].leagues[0].teams.map(a => a.team.logos[0].href)
-    console.log('fin du process...')
-    return (teamsLogo)
+async function getter(url) {
+  const res = await fetch(url);
+
+  if (res.status != 200) {
+    return [];
+  }
+
+  const resJson = await res.json();
+  return resJson;
+}
+
+async function getTeamInfo(id) {
+  const teamJson = await getter(
+    'https://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams' +
+      `/${id}`
+  );
+  return {
+    logo: teamJson.team.logos[0].href,
+    pointAgainst: Number(teamJson.team.record.items[0].stats[2].value.toFixed(1)),
+    pointScored: Number(teamJson.team.record.items[0].stats[3].value.toFixed(1))
+  };
 }
